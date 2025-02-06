0f5206cf7af3daf4c3c2d4ed34e81bc519802b3d
Sprytes0 <joeangeles@comcast.net>
2025-02-06 16:21:49 -0600

    Moved files

--- src/componets/Contact/Contact.jsx
+++ src/componets/Contact/Contact.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 
 import styles from "./Contact.module.css"
 
-import emailIcon from '../../../assets/contact/emailIcon.png';
-import githubIcon from '../../../assets/contact/githubIcon.png';
-import linkedinIcon from '../../../assets/contact/linkedinIcon.png';
-
 export const Contact = () => {
   return (
     <footer id='contact' className={styles.container}>
@@ -15,15 +11,15 @@ export const Contact = () => {
         </div>
         <ul className={styles.links}>
             <li className={styles.link}>
-                <img src={emailIcon} alt="Email icon" />
+                <img src="assets/contact/emailIcon.png" alt="Email icon" />
                 <a href="mailto:joeangeles@comcast.net">joeangeles@comcast.net</a>
             </li>
             <li className={styles.link}>
-                <img src={linkedinIcon} alt="LinkedIn icon" />
+                <img src="assets/contact/linkedinIcon.png" alt="LinkedIn icon" />
                 <a href="https://www.linkedin.com/in/joseph-angeles-3baa96249/">LinkedIn.com/JosephAngeles</a>
             </li>
             <li className={styles.link}>
-                <img src={githubIcon} alt="GitHub icon" />
+                <img src="assets/contact/githubIcon.png" alt="GitHub icon" />
                 <a href="https://github.com/Sprytes0">GitHub.com/Sprytes</a>
             </li>
         </ul>
